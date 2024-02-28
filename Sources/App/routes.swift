76e795049838dc1fba55e6c80f53f3248e9a7be5
Amish <amish.tufail2002@gmail.com>
2024-02-28 15:04:23 +0500

    - Chapter 2, Basic Roruting, GET and POST Basics, Return JSON

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,33 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    app.get("hello", "vapor") { req async -> String in
+        "Hello, VAPOR :)!!"
+    }
+    
+    app.get("hello", ":name") { req async throws -> String in
+        guard let name = req.parameters.get("name") else {
+            throw Abort(.internalServerError)
+        }
+        return "Hello, \(name). Nice to Meet You. :)"
+    }
+    
+    app.post("info") { req async throws -> String in
+        let data = try req.content.decode(InfoData.self)
+        return "Hello, \(data.name)"
+    }
+    
+    app.post("infoResponse") { req async throws -> InfoResponse in
+        let data = try req.content.decode(InfoData.self)
+        return InfoResponse(response: data)
+    }
+}
+
+struct InfoData: Content {
+    let name: String
+}
+
+struct InfoResponse: Content {
+    let response: InfoData
 }
